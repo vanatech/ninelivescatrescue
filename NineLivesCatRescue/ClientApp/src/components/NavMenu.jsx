58e83fc8b0285a8d31c8d66913473e20e2a67897
Blane Winstead <blane.wins@gmail.com>
2023-12-16 20:58:21 -0600

    Setup react with tailwind

---
+++ NineLivesCatRescue/ClientApp/src/components/NavMenu.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from 'react';
+
+export class NavMenu extends Component {
+  static displayName = NavMenu.name;
+
+  render() {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">9 Lives Cat Rescue</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <details>
+                            <summary>
+                                Adopt/Foster
+                            </summary>
+                            <ul className="p-2 bg-base-100 rounded-t-none">
+                                <li><a>Available Cats</a></li>
+                                <li><a>Adoption Application</a></li>
+                                <li><a>Foster Application</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Spay/Neuter</a></li>
+                    <li><a>About Us</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+  }
+}
