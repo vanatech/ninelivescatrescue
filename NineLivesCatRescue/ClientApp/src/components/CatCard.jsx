7c2b9456455558145eff92281bea67e0b8f6ed6e
Blane Winstead <blane.wins@gmail.com>
2023-12-17 21:23:00 -0600

    Test api in front end

--- NineLivesCatRescue/ClientApp/src/components/CatCard.jsx
+++ NineLivesCatRescue/ClientApp/src/components/CatCard.jsx
@@ -1,18 +1,38 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect} from 'react';
 
-export class CatCard extends Component {
-    render() {
-        return (
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://i.imgur.com/Jvh1OQm.jpeg" alt="Cat" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Jezabel</h2>
-                    <p>Cat description</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Adopt me</button>
-                    </div>
+const CatCard = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // Make API call when the component mounts
+        fetch('api/rescue-groups/available')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+    
+
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://i.imgur.com/Jvh1OQm.jpeg" alt="Cat" /></figure>
+            <div className="card-body">
+                <ul>
+                    {data.map((item, index) => (
+                        <li key={index}>
+                            {/* Render each node data here */}
+                            {item.name} - {item.description}
+                        </li>
+                    ))}
+                </ul>
+                <h2 className="card-title">Jezabel</h2>
+                <p>Cat description</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Adopt me</button>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+    
 }
+
+export default CatCard;
